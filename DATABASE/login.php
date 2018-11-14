4c2740e95f85f3b9325cab6c935a44ed7ff3e11a
imsatis <Malsonsatis@outlook.com>
2018-11-14 23:46:13 +0530

    file changed

--- DATABASE/login.php
+++ DATABASE/login.php
@@ -33,10 +33,17 @@ if(isset($_POST['name']) && isset($_POST['username']) && isset($_POST['email']) 
   if(isset($_POST['USERNAME']) && isset($_POST['PASSWORD'])) {
       $USERNAME=$_POST['USERNAME'];
       $PASSWORD=$_POST['PASSWORD'];
+      $data="SELECT * FROM USERDATA WHERE _USERNAME='$USERNAME'";
       $CHECK="SELECT * FROM userdata where _USERNAME='$USERNAME' AND _PASSWORD='$PASSWORD'";
       if($result=$conn->query($CHECK)){
            if($result->num_rows>0) {
-               echo "CORRECT PASSWORD";
+               $userinfo=$conn->query($data);
+               $INFO=$userinfo->fetch_assoc();
+
+               echo "<h2>!Hellow <b>$INFO[_NAME].....<b></h2><br>";
+               echo "<h3>Your Username is: <b>$INFO[_USERNAME]</b></h3><br>";
+               echo "<h3>Your Email id is: <b>$INFO[_EMAIL]</b></h3><br>";
+               
            }else echo "INCORRECT PASSWORD";  
       }else{
           echo "Query Fail";
