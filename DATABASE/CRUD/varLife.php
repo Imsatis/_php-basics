0bdc5f26e91b7706ce5b9314c6d08a60d1b34dd9
imsatis <Malsonsatis@outlook.com>
2018-11-22 23:43:25 +0530

    file changed

--- DATABASE/CRUD/varLife.php
+++ DATABASE/CRUD/varLife.php
@@ -1,11 +1,12 @@
 <?php
-
+/* 
 $var1="var1";
 
 check();
 
 if(1){
     echo $var1;
+    $var2="inside if";
 }
 
 
@@ -13,10 +14,30 @@ function check() {
     global $var1;
     //$var1="inside function";
     echo $var1;
+    $var2;
 }
 
 $var1=23.18900877;
 
 echo $var1;
+echo $var2;  */
+
+
+function check($number){
+    if($number>1){
+      throw new exception("Exception occured");
+    }
+  return true;
+}
+
+try {
+    check(1);
+    echo "NO Exception";
+}
+
+catch(exception $e){
+   echo "Message! ".$e->getMessage();
+}
+
 
 ?>
