61f3bc6afefef66d200e5a3ed0033212c91f2563
imsatis <Malsonsatis@outlook.com>
2018-11-26 21:26:20 +0530

    editing

--- DATABASE/Oops/Class.php
+++ DATABASE/Oops/Class.php
@@ -24,7 +24,65 @@ class MB {
     return $this->results;
    }
    
+  //-------------------------Read All----------------------//
 
+  function read_all($tableName) {
+    
+    $results = $this->select_all($tableName);
+    
+    $output='<table>';
+
+    $columnName='';
+    if($results->num_rows>0){
+         //print_r($columnName=array_keys($results->fetch_assoc()));
+         
+         //$columnName=array_keys($results->fetch_assoc());
+    
+         /*
+         for($i=0;$i<sizeof($columnName);$i++) {
+          $output.=$columnName[$i]." | ";
+         }
+         echo"<br>".$output;
+         
+         */
+        $check=true;
+        $j=0;
+    
+        while($rows = $results->fetch_assoc()) {
+          #echo "$rows[]<br>";
+                    if($check){
+                         $columnName = array_keys($rows);
+                         $check=false;$output .= "<tr>";
+                         for($i=0;$i<sizeof($columnName);$i++) {
+                           $output .= "<th>" . $columnName[$i]  ."</th>";
+                         }
+                         $output .= "</tr>";
+                         $j++;
+                        }
+          
+                    $output .= "<tr>";
+    
+          for($i=0;$i<sizeof($columnName);$i++) {
+               $output .= "<td>".$rows[$columnName[$i]]."</td>";
+          }
+          $output .= "<td><a href='object.php?id={$rows[$columnName[0]]}'>Update</a></td></tr>";
+        }
+        $output .= "</table>";
+         
+        //echo $output;
+        //echo $j;
+        #echo "<br>";
+        #print_r($columnName);
+        
+        /*
+        $rows = $results->fetch_array();
+        print_r($rows['_NAME']);
+        */
+     }else{
+        echo "No Record Found";
+    }
+    return $output;
+  }
 
 //-------------------------Connection Close----------------------//
 
--- DATABASE/Oops/object.php
+++ DATABASE/Oops/object.php
@@ -13,53 +13,16 @@ $obj->start_connection($dbname);
 
 //-------Select All
 
-$results = $obj->select_all($tableName);
-  $output='<table>';
-  $columnName='';
-if($results->num_rows>0){
-     //print_r($columnName=array_keys($results->fetch_assoc()));
-     
-     //$columnName=array_keys($results->fetch_assoc());
-
-     /*
-     for($i=0;$i<sizeof($columnName);$i++) {
-      $output.=$columnName[$i]." | ";
-     }
-     echo"<br>".$output;
-     
-     */
-    $check=true;
-
-    while($rows = $results->fetch_assoc()) {
-      #echo "$rows[]<br>";
-      if($check){
-        $columnName = array_keys($rows);
-       }
-       $output .= "<tr>";
-
-      for($i=0;$i<sizeof($columnName);$i++) {
-           $output .= "<td>".$rows[$columnName[$i]]."</td>";
-      }
-      $output .= "</tr>";
-    }
-    
-    echo $output;
-    #echo "<br>";
-    #print_r($columnName);
-    
-    /*
-    $rows = $results->fetch_array();
-    print_r($rows['_NAME']);
-    */
 
 
-}else{
-    echo "No Recor Found";
-}
+echo $output = $obj->read_all($tableName);
+
+#echo "heloow";
  
-  
+   
 
 
 $obj->connection_close();
+
 
 ?>
