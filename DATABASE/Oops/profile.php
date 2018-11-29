820375147d9681dd3bcdc0415c7e2f9f8c4835d9
Malson <Malsonsatis@outlook.com>
2018-11-28 18:43:52 -0800

    task in progress

---
+++ DATABASE/Oops/profile.php
@@ -0,0 +1,47 @@
+
+<?php
+
+session_start();
+
+if(isset($_GET['logout'])) {
+    unset($_SESSION['username']);
+    //header("location:loginTask.php");
+    //echo dirname(__FILE__);
+   }
+   
+
+
+if(!isset($_SESSION['username'])) {
+   header("location:loginTask.php");
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+</head>
+<body>
+    
+<div class="black"> 
+    <div class="topwrap">
+          <div class="container">
+              <div class="jumbotron">
+                   <div class="row">
+                        <div class="col-md-10">
+                             <?php echo "<h2>Welcome ". ucfirst("{$_SESSION['username']}")."!</h2>"?>
+                        </div>
+                        <div class="col-md-2">
+                         <form>
+                         <a href='profile.php'><button class="btn btn-primary" value="1" name="logout">Log out</button></a>
+                          
+                         </div>
+                   </div>
+              </div>
+          </div>
+    </div>
+</body>
+</html>
