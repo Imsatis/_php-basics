bcb23a4b15dbc42d4af8b9f4900cf17add353587
Malson <Malsonsatis@outlook.com>
2018-12-01 01:19:31 -0800

    update

--- DATABASE/Oops/fileUpload.php
+++ DATABASE/Oops/fileUpload.php
@@ -1,5 +1,23 @@
 <?php
 
+$servername="localhost";
+$username="root";
+$password="";
+$dbName="CRUD";
+
+
+//========================SERVER-CONNECTION========================
+
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+//========================DATABASE-CONNECTION======================
+
+if($conn->connect_error) {
+    echo "Connection Failed";    
+}else {
+    
+}
+
 $target='';
 
 /*
@@ -14,8 +32,8 @@ if(isset($_ary['cat'])) {
 */
 if(isset($_POST['submit'])) {
     //print_r($_FILES['file']);
-    /*
-
+    
+ /*
     $target = "uploads/".$_FILES['file']['name'];
     $path = $_FILES['file']['tmp_name'];
     //echo basename('C:\xampp\htdocs\project');
@@ -23,8 +41,12 @@ if(isset($_POST['submit'])) {
     $options = PATHINFO_DIRNAME | PATHINFO_BASENAME | PATHINFO_EXTENSION | PATHINFO_FILENAME;
     //print_r(pathinfo("Oops/uploads/1.jpg",PATHINFO_DIRNAME | PATHINFO_BASENAME | PATHINFO_EXTENSION | PATHINFO_FILENAME));
     move_uploaded_file($path,$target);
-  */
-  print_r($_POST);
+*/
+    
+    print_r($_FILES['file']); 
+    $name = $_FILES['File']['name'];
+    $data = file_get_contents($_FILES['file']['tmp_name']);
+    $type = $_FILES['file']['type'];
 
 }
 
@@ -37,14 +59,16 @@ if(isset($_POST['submit'])) {
 <body>
     
     <form action='' method='post' enctype='multipart/form-data'>
-        <input type='text' name='file'>
-        <input type='text' name='fil'>
-        <input type='text' name='fi'>
-        <input type='text' name='f'>
+        <input type='file' name='file'>
         <input type='submit' name='submit'>
     </form>
 
 </body>
 </html>
 
-<?php echo "<img src='$target' alt='no' height='' width=''/>";?>
+<?php 
+
+echo "<img src='$data' alt='no' height='' width=''/>";
+$INSERT = 'INSERT INTO ';
+$conn->query('')
+?>
